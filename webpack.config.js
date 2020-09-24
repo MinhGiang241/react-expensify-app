42aff92cce57a92e56925d3cf000b8a3853c5490
MinhGiang241 <minhgiang241@gmail.com>
2020-09-24 22:07:46 +0700

    set up dist file

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = (env) => {
     mode: "development",
     entry: "./src/app.js",
     output: {
-      path: path.join(__dirname, "public"),
+      path: path.join(__dirname, "public", "dist"),
       filename: "bundle.js",
     },
     module: {
@@ -35,7 +35,7 @@ module.exports = (env) => {
               options: {
                 esModule: true,
                 sourceMap: true,
-                publicPath: "./public",
+                publicPath: "/public/dist",
               },
             },
             {
@@ -59,6 +59,7 @@ module.exports = (env) => {
     devServer: {
       contentBase: path.join(__dirname, "public"),
       historyApiFallback: true,
+      publicPath: "/dist/",
     },
   };
 };
